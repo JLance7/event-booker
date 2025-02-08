1456ccf0952d52ca91f3cc999bd03ea9de3d4710
Josh <joshua.lanctot10@gmail.com>
2025-02-07 21:50:20 -0600

    gin

--- main.go
+++ main.go
@@ -5,19 +5,18 @@ import (
 )
 
 /*
-GET /events                   Get a list of available events
-GET /events/<id>              Get a specific event
-POST /events                  Create a new bookable event (Auth required)
-PUT /events/<id>              Update an event (Auth required, Only by creator)
-DELETE /events/<id>           Delete an event (Auth required, Only by creator)
-POST /signup                  Create a new user
-POST /login                   Authenticate a user (Auth token JWT, used for auth required routes)
-POST /events/<id>/register    Register user for an event (Auth required)
-DELETE /events/<id>/register  Cancel registration (Auth required)
-
+	GET /events                   Get a list of available events
+	GET /events/<id>              Get a specific event
+	POST /events                  Create a new bookable event (Auth required)
+	PUT /events/<id>              Update an event (Auth required, Only by creator)
+	DELETE /events/<id>           Delete an event (Auth required, Only by creator)
+	POST /signup                  Create a new user
+	POST /login                   Authenticate a user (Auth token JWT, used for auth required routes)
+	POST /events/<id>/register    Register user for an event (Auth required)
+	DELETE /events/<id>/register  Cancel registration (Auth required)
 */
 
 func main(){
 	fmt.Println("REST API!")
-
+	
 }
